a36499aca50bd71d3e17e4be9c8f7c95ee6693ae
jvill171 <juanvillarreal64@yahoo.com>
2023-06-03 21:02:43 -0500

    Added tests for routes

--- routes/itemRoutes.test.js
+++ routes/itemRoutes.test.js
@@ -5,13 +5,11 @@ const request = require("supertest");
 const app = require("../app.js");
 let items = require("../fakeDb.js");
 
-let testItem = {
-    name: "icecream",
-    price: 2.50,
-};
+let icecream = { name: "icecream", price: 2.50, };
+let item1 = { name: "cheese", price: 3.20, };
 
 beforeEach(()=>{
-    items.push(testItem);
+    items.push(icecream);
 });
 
 afterEach(()=>{
@@ -19,11 +17,75 @@ afterEach(()=>{
     items.length = 0;
 });
 
+describe("GET /badRoute", ()=>{
+    test("Attempt to acces an invalid route", async ()=>{
+        const res = await request(app).get('/badRoute')
+        
+        expect(res.statusCode).toBe(404)
+    })
+})
+
 describe("GET /items", ()=>{
     test("Gets all items", async ()=>{
         const res = await request(app).get('/items')
-        console.log(res.body)
+        
         expect(res.statusCode).toBe(200)
-
+        expect(res.body).toEqual({ "fakeDb": [icecream] })
     })
 })
+
+describe("POST /items", ()=>{
+    test("Creating a new item", async ()=>{
+        const res = await request(app).post('/items').send(item1)
+        
+        expect(res.statusCode).toBe(201)
+        expect(res.body).toEqual({ "added": item1 })
+    })
+})
+
+describe("GET /items/:name", ()=>{
+    test("Gets specific item", async ()=>{
+        const res = await request(app).get(`/items/${icecream.name}`)
+        
+        expect(res.statusCode).toBe(200)
+        expect(res.body).toEqual({ "foundItem": icecream })
+    })
+    test("Attempt to get a specific INVALID item", async ()=>{
+        const res = await request(app).get(`/items/invalidName`)
+        
+        expect(res.statusCode).toBe(404)
+    })
+})
+
+describe("PATCH /items/:name", ()=>{
+    test("Update a specific item", async ()=>{
+        const res = await request(app).patch(`/items/${icecream.name}`).send(item1)
+        
+        expect(res.statusCode).toBe(200)
+        expect(res.body).toEqual({ "updated": item1 })
+    })
+    test("Attempt to update a specific INVALID item", async ()=>{
+        const res = await request(app).patch(`/items/invalidName`).send(item1)
+        
+        expect(res.statusCode).toBe(404)
+    })
+})
+
+describe("DELETE /items/:name", ()=>{
+    test("Delete a specific item", async ()=>{
+        const res = await request(app).delete(`/items/${icecream.name}`)
+        
+        expect(res.statusCode).toBe(200)
+        expect(res.body).toEqual({"message": "Deleted"})
+    })
+    test("Delete a specific INVALID item", async ()=>{
+        const res = await request(app).delete(`/items/invalidName`)
+        
+        expect(res.statusCode).toBe(404)
+    })
+})
+
+
+
+// GET, PATCH, DELETE
+// /items/:name
